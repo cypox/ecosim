e8b910e1e741e36a041662fa5f789b989333aff4
cypox <am_neggaz@esi.dz>
2023-06-06 19:10:44 +0200

    added empty simulation from main binary

--- src/ecosim.cpp
+++ src/ecosim.cpp
@@ -8,6 +8,14 @@ int main(int argc, char** argv)
   bool no_stepping = parser.exists("--no-stepping");
   int framerate = parser.get_int("--frame-rate", 60);
   int rng_seed = parser.get_long("--seed", time(nullptr));
+  int simulation_steps = parser.get_int("--steps", 1000);
+
+  World w;
+
+  while(--simulation_steps)
+  {
+    w.step();
+  }  
 
   return 0;
 }
