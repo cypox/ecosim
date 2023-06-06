50fb961e155eb118f1e2b47c67ebce00aed57e4a
cypox <am_neggaz@esi.dz>
2023-06-06 19:04:43 +0200

    added world stub

---
+++ include/world.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "entity.hpp"
+#include "exchange.hpp"
+
+
+class World {
+public:
+  World();
+
+  void step();
+
+private:
+  Exchange _market; // TODO: when simulating a big market, whis will become a set of exchanges
+  std::vector<Entity> _entities;
+};
---
+++ src/world.cpp
@@ -0,0 +1,12 @@
+#include "include/world.hpp"
+
+
+World::World()
+{
+  return;
+}
+
+void World::step()
+{
+  return;
+}
