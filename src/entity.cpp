7d46685427a8bdb1f9f5a3b187c97549c98df753
cypox <am_neggaz@esi.dz>
2023-06-06 17:41:41 +0200

    added entity stubs

---
+++ src/entity.cpp
@@ -0,0 +1,22 @@
+#include "include/entity.hpp"
+
+
+Entity::Entity()
+{
+  return;
+}
+
+void Entity::make_order()
+{
+  return;
+}
+
+void Entity::produce()
+{
+  return;
+}
+
+void Entity::consume()
+{
+  return;
+}
